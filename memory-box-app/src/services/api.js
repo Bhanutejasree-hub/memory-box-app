f3ec412fae7b4c10580494731e6489c904909583
Bhanutejasree-hub <bhanutejasree@gmail.com>
2026-06-20 22:02:52 +0530

    Update backend URL for Render deployment

--- memory-box-app/src/services/api.js
+++ memory-box-app/src/services/api.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
+<<<<<<< HEAD
   baseURL: "https://online-memory.onrender.com"
+=======
+  baseURL: "https://online-memory.onrender.com",
+>>>>>>> 0c67738 (Update backend URL for Render deployment)
 });
 
 export default api;
